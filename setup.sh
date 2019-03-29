d7453b4ffef91df5b83c24778fc80e1387d5aec8
ElderMael <sftwr.mael@gmail.com>
2019-03-29 11:26:11 -0600

    Add SDKMan Setup

--- setup.sh
+++ setup.sh
@@ -19,4 +19,8 @@ git config --global user.email "sftwr.mael@gmail.com"
 git config --global user.name "ElderMael"
 git config --global core.editor "emacs"
 
+set +x # Next command is very verbose!
+curl -s "https://get.sdkman.io" | zsh
+set -x
+
 /usr/bin/env zsh ./install
